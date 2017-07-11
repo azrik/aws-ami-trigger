645653fbb9ea046c2ed07dbaca64e57668b449de
Rik Turnbull <azrik@users.noreply.github.com>
2017-07-11 19:07:06 +0100

    add parameterized testing

--- src/test/java/hudson/plugins/awsamitrigger/AwsAmiTriggerCauseTest.java
+++ src/test/java/hudson/plugins/awsamitrigger/AwsAmiTriggerCauseTest.java
@@ -28,7 +28,9 @@ import com.amazonaws.services.ec2.model.ProductCode;
 import com.amazonaws.services.ec2.model.Tag;
 import com.amazonaws.util.DateUtils;
 
+import java.util.Arrays;
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.Date;
 import java.util.List;
 
@@ -38,7 +40,11 @@ import org.apache.commons.lang.StringUtils;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameter;
+import org.junit.runners.Parameterized.Parameters;
 import org.powermock.modules.junit4.PowerMockRunner;
+import org.powermock.modules.junit4.PowerMockRunnerDelegate;
 
 /**
  * Run tests for {@link AwsAmiTriggerCause}.
@@ -47,58 +53,128 @@ import org.powermock.modules.junit4.PowerMockRunner;
  *
  */
 @RunWith(PowerMockRunner.class)
+@PowerMockRunnerDelegate(value = Parameterized.class)
 public class AwsAmiTriggerCauseTest extends AwsAmiAbstractTest {
 
   private final static String SHORT_DESCRIPTION_PREFIX = "Started due to new matching image(s): ";
 
-  /**
-   * Test that nothing fails if no matches are added.
-   */
-  @Test
-  public void testPopulateEnvironmentNone() {
-    testPopulateEnvironment(0);
-  }
+  @Parameter(0)
+  public String filterArchitecture;
+  @Parameter(1)
+  public String filterDescription;
+  @Parameter(2)
+  public String filterName;
+  @Parameter(3)
+  public String filterOwnerAlias;
+  @Parameter(4)
+  public String filterOwnerId;
+  @Parameter(5)
+  public String filterProductCode;
+  @Parameter(6)
+  public String filterTags;
+  @Parameter(7)
+  public String filterShared;
+  @Parameter(8)
+  public String imageArchitecture;
+  @Parameter(9)
+  public Date imageCreationDate;
+  @Parameter(10)
+  public String imageDescription;
+  @Parameter(11)
+  public String imageName;
+  @Parameter(12)
+  public String imageOwnerAlias;
+  @Parameter(13)
+  public String imageOwnerId;
+  @Parameter(14)
+  public String imageProductCode;
+  @Parameter(15)
+  public String imageTags;
+  @Parameter(16)
+  public String imageShared;
+  @Parameter(17)
+  public String imageHypervisor;
+  @Parameter(18)
+  public String imageId;
+  @Parameter(19)
+  public String imageType;
+  @Parameter(20)
+  public String imageTagKey;
+  @Parameter(21)
+  public String imageTagValue;
+  @Parameter(22)
+  public int testMatches;
+  @Parameter(23)
+  public String expectedImageDescription;
 
   /**
-   * Test that the environment variables are populated correctly when
-   * only one match is added.
+   * Initializes test data.
+   *
+   * @return a collection of test data to populate the parameter fields
    */
-  @Test
-  public void testPopulateEnvironmentSingle() {
-    testPopulateEnvironment(1);
-  }
-
-  /**
-   * Test that the environment variables are populated correctly when
-   * multiple matches are added.
-   */
-  @Test
-  public void testPopulateEnvironmentMultiple() {
-    testPopulateEnvironment(2);
+  @Parameters
+  public static Collection<Object[]> data() {
+    return Arrays.asList(
+      new Object[][] {
+        {
+          /* filter */
+          ARCHITECTURE, DESCRIPTION, NAME, OWNER_ALIAS, OWNER_ID, PRODUCT_CODE, TAGS, SHARED,
+           /* image */
+          ARCHITECTURE, new Date(System.currentTimeMillis()+(1000*60*30)), DESCRIPTION, NAME,
+          OWNER_ALIAS, OWNER_ID, PRODUCT_CODE, TAGS, SHARED, HYPERVISOR, IMAGE_ID, IMAGE_TYPE,
+          TAG_KEY, TAG_VALUE,
+          /* test runs */
+          0,
+          /* expected values */
+          DESCRIPTION
+        },
+        {
+          /* filter */
+          ARCHITECTURE, DESCRIPTION, NAME, OWNER_ALIAS, OWNER_ID, PRODUCT_CODE, TAGS, SHARED,
+          /* image */
+          ARCHITECTURE, new Date(), DESCRIPTION, NAME, OWNER_ALIAS, OWNER_ID, PRODUCT_CODE,
+          TAGS, SHARED, HYPERVISOR, IMAGE_ID, IMAGE_TYPE, TAG_KEY, TAG_VALUE,
+          /* test runs */
+          1,
+          /* expected values */
+          DESCRIPTION
+        },
+        {
+          /* filter */
+          ARCHITECTURE, DESCRIPTION, NAME, OWNER_ALIAS, OWNER_ID, PRODUCT_CODE, TAGS, SHARED,
+          /* image */
+          ARCHITECTURE, new Date(), null, NAME, OWNER_ALIAS, OWNER_ID, PRODUCT_CODE, TAGS, SHARED,
+          HYPERVISOR, IMAGE_ID, IMAGE_TYPE, TAG_KEY, TAG_VALUE,
+          /* test runs */
+          2,
+          /* expected values */
+          ""
+        },
+      }
+    );
   }
 
   /**
    * Tests that the environment variables are populated correctly
    * for a certain number of matches.
-   *
-   * @param matches  number of matches to check for
    */
-  private void testPopulateEnvironment(int matches) {
+  @Test
+  public void testPopulateEnvironment() {
     AwsAmiTriggerCause cause = new AwsAmiTriggerCause();
-    for(int i = 0; i < matches; i++) {
-      cause.addMatch(getFilter(), getImage());
+    for(int i = 0; i < testMatches; i++) {
+      cause.addMatch(createFilter(), createImage());
     }
 
     EnvVars envVars = new EnvVars();
     cause.populateEnvironment(envVars);
 
     List<String> imageIds = new ArrayList<String>();
-    for(int i = 0; i < matches; i++) {
+    for(int i = 0; i < testMatches; i++) {
       imageIds.add(IMAGE_ID);
     }
 
     Assert.assertEquals("getShortDescription()", SHORT_DESCRIPTION_PREFIX + StringUtils.join(imageIds, ","), cause.getShortDescription());
-    for(int i = 0; i < matches; i++) {
+    for(int i = 0; i < testMatches; i++) {
       assertEnvironmentVariablesSet(envVars, i+1);
     }
   }
@@ -110,44 +186,46 @@ public class AwsAmiTriggerCauseTest extends AwsAmiAbstractTest {
    * @param suffix   suffix for environment variable names
    */
   private void assertEnvironmentVariablesSet(EnvVars envVars, int suffix) {
-    Assert.assertEquals("envImageArchitecture", ARCHITECTURE, envVars.get("awsAmiTriggerImageArchitecture" + suffix));
-    Assert.assertEquals("envImageCreationDate", DateUtils.formatISO8601Date(CREATION_DATE), envVars.get("awsAmiTriggerImageCreationDate" + suffix));
-    Assert.assertEquals("envImageDescription", DESCRIPTION, envVars.get("awsAmiTriggerImageDescription" + suffix));
-    Assert.assertEquals("envImageHypervisor", HYPERVISOR, envVars.get("awsAmiTriggerImageHypervisor" + suffix));
-    Assert.assertEquals("envImageId", IMAGE_ID, envVars.get("awsAmiTriggerImageId" + suffix));
-    Assert.assertEquals("envImageType", IMAGE_TYPE, envVars.get("awsAmiTriggerImageType" + suffix));
-    Assert.assertEquals("envImageName", NAME, envVars.get("awsAmiTriggerImageName" + suffix));
-    Assert.assertEquals("envImageOwnerAlias", OWNER_ALIAS, envVars.get("awsAmiTriggerImageOwnerAlias" + suffix));
-    Assert.assertEquals("envImageOwnerId", OWNER_ID, envVars.get("awsAmiTriggerImageOwnerId" + suffix));
-    Assert.assertEquals("envImageProductCodes", PRODUCT_CODE, envVars.get("awsAmiTriggerImageProductCodes" + suffix));
-    Assert.assertEquals("envImageTags", TAGS, envVars.get("awsAmiTriggerImageTags" + suffix));
-    Assert.assertEquals("envImageIsPublic", SHARED, envVars.get("awsAmiTriggerImageIsPublic" + suffix));
+    Assert.assertEquals("envImageArchitecture", imageArchitecture, envVars.get("awsAmiTriggerImageArchitecture" + suffix));
+    Assert.assertEquals("envImageCreationDate", DateUtils.formatISO8601Date(imageCreationDate), envVars.get("awsAmiTriggerImageCreationDate" + suffix));
+    Assert.assertEquals("envImageDescription", expectedImageDescription, envVars.get("awsAmiTriggerImageDescription" + suffix));
+    Assert.assertEquals("envImageHypervisor", imageHypervisor, envVars.get("awsAmiTriggerImageHypervisor" + suffix));
+    Assert.assertEquals("envImageId", imageId, envVars.get("awsAmiTriggerImageId" + suffix));
+    Assert.assertEquals("envImageType", imageType, envVars.get("awsAmiTriggerImageType" + suffix));
+    Assert.assertEquals("envImageName", imageName, envVars.get("awsAmiTriggerImageName" + suffix));
+    Assert.assertEquals("envImageOwnerAlias", imageOwnerAlias, envVars.get("awsAmiTriggerImageOwnerAlias" + suffix));
+    Assert.assertEquals("envImageOwnerId", imageOwnerId, envVars.get("awsAmiTriggerImageOwnerId" + suffix));
+    Assert.assertEquals("envImageProductCodes", imageProductCode, envVars.get("awsAmiTriggerImageProductCodes" + suffix));
+    Assert.assertEquals("envImageTags", imageTags, envVars.get("awsAmiTriggerImageTags" + suffix));
+    Assert.assertEquals("envImageIsPublic", imageShared, envVars.get("awsAmiTriggerImageIsPublic" + suffix));
 
-    Assert.assertEquals("envFilterArchitecture", ARCHITECTURE, envVars.get("awsAmiTriggerFilterArchitecture" + suffix));
-    Assert.assertEquals("envFilterDescription", DESCRIPTION, envVars.get("awsAmiTriggerFilterDescription" + suffix));
-    Assert.assertEquals("envFilterName", NAME, envVars.get("awsAmiTriggerFilterName" + suffix));
-    Assert.assertEquals("envFilterOwnerAlias", OWNER_ALIAS, envVars.get("awsAmiTriggerFilterOwnerAlias" + suffix));
-    Assert.assertEquals("envFilterOwnerId", OWNER_ID, envVars.get("awsAmiTriggerFilterOwnerId" + suffix));
-    Assert.assertEquals("envFilterProductCode", PRODUCT_CODE, envVars.get("awsAmiTriggerFilterProductCode" + suffix));
-    Assert.assertEquals("envFilterTags", TAGS, envVars.get("awsAmiTriggerFilterTags" + suffix));
-    Assert.assertEquals("envFilterIsPublic", SHARED, envVars.get("awsAmiTriggerFilterIsPublic" + suffix));
+    Assert.assertEquals("envFilterArchitecture", filterArchitecture, envVars.get("awsAmiTriggerFilterArchitecture" + suffix));
+    Assert.assertEquals("envFilterDescription", filterDescription, envVars.get("awsAmiTriggerFilterDescription" + suffix));
+    Assert.assertEquals("envFilterName", filterName, envVars.get("awsAmiTriggerFilterName" + suffix));
+    Assert.assertEquals("envFilterOwnerAlias", filterOwnerAlias, envVars.get("awsAmiTriggerFilterOwnerAlias" + suffix));
+    Assert.assertEquals("envFilterOwnerId", filterOwnerId, envVars.get("awsAmiTriggerFilterOwnerId" + suffix));
+    Assert.assertEquals("envFilterProductCode", filterProductCode, envVars.get("awsAmiTriggerFilterProductCode" + suffix));
+    Assert.assertEquals("envFilterTags", filterTags, envVars.get("awsAmiTriggerFilterTags" + suffix));
+    Assert.assertEquals("envFilterIsPublic", filterShared, envVars.get("awsAmiTriggerFilterIsPublic" + suffix));
   }
 
   /**
-   * Gets a filter based on the constant test values.
-   * @return a filter
-   */
-  private AwsAmiTriggerFilter getFilter() {
-    return createFilter(ARCHITECTURE, DESCRIPTION,
-      NAME, OWNER_ALIAS, OWNER_ID, PRODUCT_CODE, TAGS, SHARED);
-  }
-
-  /**
-   * Gets an image based on the constant values.
+   * Creates an image based on the test values.
+   *
    * @return an image
    */
-  private Image getImage() {
-    return createImage(ARCHITECTURE, CREATION_DATE, DESCRIPTION, HYPERVISOR,
-      IMAGE_ID, IMAGE_TYPE, NAME, OWNER_ALIAS, OWNER_ID, PRODUCT_CODE, TAG_KEY, TAG_VALUE, SHARED);
+  private Image createImage() {
+    return createImage(imageArchitecture, imageCreationDate, imageDescription, imageHypervisor, imageId,
+      imageType, imageName, imageOwnerAlias, imageOwnerId, imageProductCode, imageTagKey, imageTagValue, imageShared);
+  }
+
+  /**
+   * Creates a filter based on the test values.
+   *
+   * @return a filter
+   */
+  private AwsAmiTriggerFilter createFilter() {
+    return createFilter(filterArchitecture, filterDescription, filterName,
+      filterOwnerAlias, filterOwnerId, filterProductCode, filterTags, filterShared);
   }
 }
